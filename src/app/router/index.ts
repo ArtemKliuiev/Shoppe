f89fb2e60ee9f9843b7dab02dd226a495dc11dff
VUE Artem Kluev <mr.kluev9999@gmail.com>
2025-02-27 16:41:53 +0200

    edit

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('https://my-shoppe-shop.netlify.app/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
