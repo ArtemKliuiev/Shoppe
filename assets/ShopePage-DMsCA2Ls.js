25bd12d759f8c6689021023af7fe774e60dc266f
VUE Artem Kluev <mr.kluev9999@gmail.com>
2025-02-28 12:03:42 +0200

    Updates

--- assets/ShopePage-DMsCA2Ls.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-Qe9FuGME.js";const r={};function c(o,t){return" Shope Page "}const n=e(r,[["render",c]]);export{n as default};
---
+++ assets/ShopePage-DeFBb_rZ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-CwQlCHRb.js";const r={};function c(o,t){return" Shope Page "}const n=e(r,[["render",c]]);export{n as default};
