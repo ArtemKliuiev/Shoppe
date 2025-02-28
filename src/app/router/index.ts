587b6a30ca91be74b979aa8c0dd91362f07297fc
VUE Artem Kluev <mr.kluev9999@gmail.com>
2025-02-28 13:53:15 +0200

    test deploy

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter,  createWebHistory } from 'vue-router'
 import MainPage from '@/components/pages/main/MainPage.vue'
 import ShopePage from '@/components/pages/shope/ShopePage.vue'
 import ProductPage from '@/components/pages/product/ProductPage.vue'
 
 const router = createRouter({
-  history: createWebHashHistory('/Shoppe/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
