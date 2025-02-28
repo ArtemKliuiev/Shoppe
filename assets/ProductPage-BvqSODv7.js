84bd4b72d20b22f658f1abe228a1280dce6edeb3
VUE Artem Kluev <mr.kluev9999@gmail.com>
2025-02-28 12:18:21 +0200

    Updates

---
+++ assets/ProductPage-BvqSODv7.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-By9Jgqr8.js";const c={};function e(t,o){return" Product Page "}const n=r(c,[["render",e]]);export{n as default};
--- assets/ProductPage-DyQsBRd9.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-CAjtmN08.js";const c={};function e(t,o){return" Product Page "}const n=r(c,[["render",e]]);export{n as default};
